c11c48514610c165832c027c60548d524ffca130
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-15 09:12:57 -0700

    test #initialize w/ exists & has attributes tests

---
+++ spec/student_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/student'
+
+RSpec.describe Student do
+  describe '#initialize' do 
+    it 'exists' do 
+      student = Student.new({name: "Morgan", age: 21})
+      expect(student).to be_a(Student)
+    end
+
+    it 'has attributes' do 
+      student = Student.new({name: "Morgan", age: 21})
+      expect(student.name).to eq("Morgan")
+      expect(student.age).to eq(21)
+      expect(student.scores).to eq([])
+    end
+  end
+end
