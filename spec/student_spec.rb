da143918796256798a662c79f4b2a0a26382e616
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-15 10:31:30 -0700

    refactor tests

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,33 +2,35 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  before(:each) do 
-    @student = Student.new({name: "Morgan", age: 21})
-  end
+  # before(:each) do 
+  #   student = Student.new({name: "Morgan", age: 21})
+  # end
   describe '#initialize' do 
     it 'exists' do 
-      # @student = Student.new({name: "Morgan", age: 21})
-      expect(@student).to be_a(Student)
+      student = Student.new({name: "Morgan", age: 21})
+      expect(student).to be_a(Student)
     end
 
     it 'has attributes' do 
-      # @student = Student.new({name: "Morgan", age: 21})
-      expect(@student.name).to eq("Morgan")
-      expect(@student.age).to eq(21)
-      expect(@student.scores).to eq([])
+      student = Student.new({name: "Morgan", age: 21})
+      expect(student.name).to eq("Morgan")
+      expect(student.age).to eq(21)
+      expect(student.scores).to eq([])
     end
   end
 
   describe '#logs' do
     it 'logs student scores in @scores hash' do
-      @student.log_score(89)
-      @student.log_score(78)
+      student = Student.new({name: "Morgan", age: 21})
+      student.log_score(89)
+      student.log_score(78)
       expect(student.scores).to eq([89, 78])
     end
 
-    it 'calculates student average score'
-      @student.log_score(89)
-      @student.log_score(78)
-      expect(student.grade).to eq(83.5)
+    # xit 'calculates student average score'
+    #   student = Student.new({name: "Morgan", age: 21})  
+    #   student.log_score(89)
+    #   student.log_score(78)
+    #   expect(student.grade).to eq(83.5)
   end
 end
