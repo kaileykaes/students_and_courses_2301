c1be8e1bc1d7578c04edc17e990b01f2e3ac0495
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-15 10:46:37 -0700

    test enrolls and has capacity

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -12,5 +12,23 @@ RSpec.describe Course do
       course = Course.new("Calculus", 2)
       expect(course.students).to eq([])
     end
+
+    it 'enrolls students' do 
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)
+      course.enroll(student2)
+      expect(course.students).not_to eq(nil)
+    end
+
+    it 'is full at capacity' do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)
+      course.enroll(student2)
+      expect(course.full?).to eq(true)
+    end
   end
 end
